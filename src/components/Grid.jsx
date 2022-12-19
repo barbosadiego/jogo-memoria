73ded1cee364b4e23b094c004c01ef722550f88c
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-19 16:04:18 -0300

    duplicate map of icons and add styling

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import Item from './Item';
 import icons from '../assets/iconsURL.json';
+import styled from 'styled-components';
 
 const Grid = () => {
   return (
-    <div>
-      <p>Grid</p>
-      <div>
-        {icons && icons.map((image) => <Item key={image.id} image={image} />)}
-      </div>
-    </div>
+    <StyledGrid>
+      {icons &&
+        icons.map((image) => (
+          <div>
+            <Item key={image.id} image={image} />
+          </div>
+        ))}
+      {icons &&
+        icons.map((image) => (
+          <div>
+            <Item key={image.id} image={image} />
+          </div>
+        ))}
+    </StyledGrid>
   );
 };
 
 export default Grid;
+
+const StyledGrid = styled.div`
+  display: grid;
+  justify-items: center;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+  gap: 0.5rem;
+`;
