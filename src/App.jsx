b5a8f7a86144289dc364d2d0c6fc055fc0b508cb
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-22 11:26:00 -0300

    new logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-import Container from './components/Container';
-import Grid from './components/Grid';
 
 const App = () => {
-  return (
-    <>
-      <Container>
-        <Title>Jogo da Memória</Title>
-        <Grid />
-      </Container>
-    </>
-  );
+  return <div>App</div>;
 };
 
 export default App;
-
-const Title = styled.h1`
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  font-size: clamp(1.5rem, 5vw, 5rem);
-  text-align: center;
-`;
